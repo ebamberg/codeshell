77a61aa4d237202aa66c327d177db84ffd488d41
Erik Bamberg <erik.bamberg@web.de>
2024-06-12 17:24:30 +0100

    Batch script support

--- app/cmd/execute.go
+++ app/cmd/execute.go
@@ -18,6 +18,7 @@ var executeCmd = &cobra.Command{
 	    execute <scriptname>`,
 	Args:    cobra.ExactArgs(1),
 	Example: "execute mybatch.script",
+
 	Run: func(cmd *cobra.Command, args []string) {
 		scriptname := args[0]
 		shell.ExecuteScript(scriptname)
@@ -26,14 +27,4 @@ var executeCmd = &cobra.Command{
 
 func init() {
 	rootCmd.AddCommand(executeCmd)
-
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// executeCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// executeCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
